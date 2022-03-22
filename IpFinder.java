46886b430386f35f8c493f41a4e71924660e59bd
Akshay Chouke <akshaychauke50@gmail.com>
2022-03-22 23:42:11 +0530

    Add files via upload

---
+++ IpFinder.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.net.*;
+
+public class IpFinder extends JFrame implements ActionListener{
+	JLabel l;
+	JTextField tf; 
+	JButton b;
+	
+	public IpFinder() {
+		super("Ip finder in this section");
+		l = new JLabel("Enter your Address or url here ");
+		l.setBounds(50,70,150,20);
+		
+		tf = new JTextField();
+		
+		tf.setBounds(50,100,200,20);
+		b = new JButton("Finder your IP address");
+		b.setBounds(50, 150,80, 30);
+		b.addActionListener(this);
+		add(l);
+		add(tf);
+		add(b);
+		
+		setSize(300,300);
+		setLayout(null);
+		setVisible(true);
+			
+	}
+	public void actionPerformed(ActionEvent e) {
+		String url = tf.getText();
+		
+		try {
+			InetAddress ia = InetAddress.getByName(url);
+			String ip = ia.getHostAddress();
+			JOptionPane.showMessageDialog(this, ip);
+		}
+		catch(UnknownHostException e1 ) {
+			JOptionPane.showMessageDialog(this, e1.toString());
+		}
+	}
+	public static void main(String args[]) {
+		new IpFinder();
+	}
+
+}
